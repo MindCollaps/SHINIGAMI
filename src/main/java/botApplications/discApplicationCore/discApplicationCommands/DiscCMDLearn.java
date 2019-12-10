195d7d8cc9d27a3736c9a899cbe98e79fb2b9471
NeoMC2 <NTill@gmx.de>
2019-12-10 20:06:07 +0100

    fix

--- src/main/java/botApplications/discApplicationCore/discApplicationCommands/DiscCMDLearn.java
+++ src/main/java/botApplications/discApplicationCore/discApplicationCommands/DiscCMDLearn.java
@@ -9,13 +9,12 @@ import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
 
 import java.awt.*;
-import java.util.ArrayList;
 
-public class DiscCMDLearn implements DicCommand{
+public class DiscCMDLearn implements DicCommand {
     @Override
     public boolean calledServer(String[] args, GuildMessageReceivedEvent event, DiscApplicationServer server, DiscApplicationUser user, Engine engine) {
         //if(server.isLearnServer()){
-            return true;
+        return true;
 
             /*
         } else {
@@ -27,102 +26,94 @@ public class DiscCMDLearn implements DicCommand{
 
     @Override
     public void actionServer(String[] args, GuildMessageReceivedEvent event, DiscApplicationServer server, DiscApplicationUser user, Engine engine) {
-        switch (args[0].toLowerCase()){     //TODO: Remove this!
-            case "date":
-            case "appointment":
-            case "apm":
+        if (args.length > 0) {
+            switch (args[0].toLowerCase()) {
+                case "add":
+                    if (args.length > 0) {
+                        switch (args[1]) {
+                            case "apm":
+                                String date;
+                                String headline = "";
+                                String subject;
+                                String description = "";
 
-                if(args.length>0){
-                    switch (args[1].toLowerCase()){
-                        case "add":
-                            if(args.length>1){
-                                switch (args[2]){
-                                    case "apm":
-                                        String date;
-                                        String headline = "";
-                                        String subject;
-                                        String description = "";
-
-                                        if(args.length>5){
-                                            date = args[3];
-                                            headline = args[4];
-                                            subject = args[5];
-                                            for (int i = 6; i < args.length; i++) {
-                                                description = description + " " + args[i];
-                                            }
-                                            try {
-                                                engine.getDiscEngine().getFilesHandler().getServerById(event.getGuild().getId()).addAppointment(date + " " + headline + " " + subject + " " + description);
-                                                engine.getDiscEngine().getTextUtils().sendSucces("Termin " + headline + " erfolgreich erstellt!", event.getChannel(), engine.getProperties().getMiddleTime());
-                                            } catch (Exception e) {
-                                                e.printStackTrace();
-                                                engine.getDiscEngine().getTextUtils().sendError("Dieser Server hat keinen Apm Channel oder ist nicht registriertl!", event.getChannel(), engine.getProperties().getMiddleTime(), false);
-                                            }
-                                        }
-                                        break;
+                                if (args.length > 4) {
+                                    date = args[2];
+                                    headline = args[3];
+                                    subject = args[4];
+                                    for (int i = 5; i < args.length; i++) {
+                                        description = description + " " + args[i];
+                                    }
+                                    try {
+                                        engine.getDiscEngine().getFilesHandler().getServerById(event.getGuild().getId()).addAppointment(date + " " + headline + " " + subject + " " + description);
+                                        engine.getDiscEngine().getTextUtils().sendSucces("Termin " + headline + " erfolgreich erstellt!", event.getChannel(), engine.getProperties().getMiddleTime());
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                        engine.getDiscEngine().getTextUtils().sendError("Dieser Server hat keinen Apm Channel oder ist nicht registriertl!", event.getChannel(), engine.getProperties().getMiddleTime(), false);
+                                    }
                                 }
-                            }
-                            break;
+                                break;
+                        }
+                    }
+                    break;
 
-                        case "remove":
+                case "remove":
 
-                            if(args.length>1){
-                                switch (args[2].toLowerCase()){
-                                    case "apmchannel":
+                    if (args.length > 0) {
+                        switch (args[1].toLowerCase()) {
+                            case "apmchannel":
+                                try {
+                                    engine.getDiscEngine().getFilesHandler().getServerById(event.getGuild().getId()).setAppointmentChannel("");
+                                } catch (Exception e) {
+                                    engine.getDiscEngine().getTextUtils().sendError("Dieser Server hat keinen Apm Channel!", event.getChannel(), engine.getProperties().getMiddleTime(), false);
+                                    return;
+                                }
+                                engine.getDiscEngine().getTextUtils().sendSucces("Der Channel wurde entfernt!", event.getChannel(), engine.getProperties().getMiddleTime());
+                                break;
+                        }
+                    }
+                    break;
+
+                case "set":
+                    if (args.length > 0) {
+                        switch (args[1].toLowerCase()) {
+                            case "apmchannel":
+                                if (args.length > 1) {
+                                    TextChannel tc;
+                                    DiscApplicationServer s = null;
+                                    try {
+                                        tc = event.getGuild().getTextChannelById(args[2]);
+                                    } catch (Exception e) {
+                                        engine.getDiscEngine().getTextUtils().sendError("Invalid Channel!", event.getChannel(), engine.getProperties().getMiddleTime(), false);
+                                        return;
+                                    }
+                                    try {
+                                        s = engine.getDiscEngine().getFilesHandler().getServers().get(event.getGuild().getId());
+                                    } catch (Exception e) {
                                         try {
-                                            engine.getDiscEngine().getFilesHandler().getServerById(event.getGuild().getId()).setAppointmentChannel("");
-                                        } catch (Exception e) {
-                                            engine.getDiscEngine().getTextUtils().sendError("Dieser Server hat keinen Apm Channel!", event.getChannel(), engine.getProperties().getMiddleTime(), false);
+                                            engine.getDiscEngine().getFilesHandler().createNewServer(event.getGuild());
+                                        } catch (Exception ex) {
+                                            ex.printStackTrace();   //KANN NICHT PASSIEREN O-O eigentlich
                                             return;
                                         }
-                                        engine.getDiscEngine().getTextUtils().sendSucces("Der Channel wurde entfernt!", event.getChannel(), engine.getProperties().getMiddleTime());
-                                        break;
+                                    }
+                                    s.setAppointmentChannel(tc.getId());
+                                    engine.getDiscEngine().getTextUtils().sendSucces("Dieser Channel wird nun als Terminchannel verwendet!", tc, engine.getProperties().getMiddleTime());
+                                } else {
+                                    engine.getDiscEngine().getTextUtils().sendError("Invalid Channel!", event.getChannel(), engine.getProperties().getMiddleTime(), false);
                                 }
-                            }
-                            break;
-
-                        case "set":
-                            if(args.length>1){
-                                switch (args[2].toLowerCase()){
-                                    case "apmchannel":
-                                        if(args.length>2){
-                                            TextChannel tc;
-                                            DiscApplicationServer s = null;
-                                            try{
-                                                tc = event.getGuild().getTextChannelById(args[3]);
-                                            } catch (Exception e){
-                                                engine.getDiscEngine().getTextUtils().sendError("Invalid Channel!", event.getChannel(), engine.getProperties().getMiddleTime(), false);
-                                                return;
-                                            }
-                                            try {
-                                                s = engine.getDiscEngine().getFilesHandler().getServers().get(event.getGuild().getId());
-                                            } catch (Exception e){
-                                                try {
-                                                    engine.getDiscEngine().getFilesHandler().createNewServer(event.getGuild());
-                                                } catch (Exception ex) {
-                                                    ex.printStackTrace();   //KANN NICHT PASSIEREN O-O eigentlich
-                                                    return;
-                                                }
-                                            }
-                                            s.setAppointmentChannel(tc.getId());
-                                            engine.getDiscEngine().getTextUtils().sendSucces("Dieser Channel wird nun als Terminchannel verwendet!", tc, engine.getProperties().getMiddleTime());
-                                        } else {
-                                            engine.getDiscEngine().getTextUtils().sendError("Invalid Channel!", event.getChannel(), engine.getProperties().getMiddleTime(), false);
-                                        }
-                                        break;
-                                }
-                            }
-                            break;
-
-                        case "print":
-                        case "renew":
-                        case "reload":
-                        case "rl":
-                            renewApmChannel(server, event, engine);
-                            break;
+                                break;
+                        }
                     }
-                }
+                    break;
 
-                break;
+                case "print":
+                case "renew":
+                case "reload":
+                case "rl":
+                    renewApmChannel(server, event, engine);
+                    break;
+            }
         }
     }
 
@@ -139,9 +130,9 @@ public class DiscCMDLearn implements DicCommand{
 
     @Override
     public String help(Engine engine) {
-        return "-learn apm add apm <datum> <headline> <Fach> <Beschreibung> -Erstellt neuen Termin\n" +
-                "-learn apm set apmchannel <channel id> -Stellt den Termin Channel ein\n" +
-                "-learn [print/renew/reload/rl] -Lädt den Terminchannel neu";
+        return "-learn add apm <datum> <headline> <Fach> <Beschreibung> - Erstellt neuen Termin\n" +
+                "-learn set apmchannel <channel id> - Stellt den Termin Channel ein\n" +
+                "-learn [print/renew/reload/rl] - Lädt den Terminchannel neu";
     }
 
     @Override
@@ -149,19 +140,23 @@ public class DiscCMDLearn implements DicCommand{
         //Nothing
     }
 
-    private void renewApmChannel(DiscApplicationServer server, GuildMessageReceivedEvent event, Engine engine){
+    private void renewApmChannel(DiscApplicationServer server, GuildMessageReceivedEvent event, Engine engine) {
         String currentApm = "";
-        String dateParsed = "";
         Object[] sortApms = server.getAppointments().toArray();
         for (int i = 1; i < sortApms.length; i++) {
-            for (int j = 0; j < sortApms.length-1; j++) {
-                if(parseDate((String) sortApms[j])>parseDate((String) sortApms[j+1])){
-                    currentApm = (String) sortApms[j];
-                    sortApms[j] = sortApms [j+1];
-                    sortApms[j+1] = currentApm;
+            for (int j = 0; j < sortApms.length - 1; j++) {
+                try {
+                    if (parseDate((String) sortApms[j]) > parseDate((String) sortApms[j + 1])) {
+                        currentApm = (String) sortApms[j];
+                        sortApms[j] = sortApms[j + 1];
+                        sortApms[j + 1] = currentApm;
+                    }
+                } catch (Exception e) {
+                    System.err.println("---following error is caused by Date parse error---");
+                    e.printStackTrace();
                 }
             }
-            
+
         }
 
         String[] apms = new String[sortApms.length];
@@ -169,16 +164,17 @@ public class DiscCMDLearn implements DicCommand{
             apms[i] = (String) sortApms[i];
         }
 
-        for (String apm:apms) {
+        for (String apm : apms) {
             engine.getDiscEngine().getTextUtils().sendCustomMessage(buildAppointment(apm), event.getGuild().getTextChannelById(server.getAppointmentChannel()), "Appointment", Color.BLUE);
         }
     }
-    
-    private int parseDate(String date){
-        return Integer.valueOf(date.split("\\.")[2].substring(0,3) + date.split("\\.")[1] + date.split("\\.")[0]);
+
+    private int parseDate(String date) throws Exception{
+        String da = date.split("\\.")[2].substring(0, 4) + date.split("\\.")[1] + date.split("\\.")[0];
+        return Integer.valueOf(da);
     }
 
-    private String buildAppointment(String raw){
+    private String buildAppointment(String raw) {
         String[] parts = raw.split(" ");
         String des = "";
         for (int i = 3; i < parts.length; i++) {
